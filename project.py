6846ba3823ce8da630dfba775f204036728a1edb
Fábio <fabioovila@gmail.com>
2026-07-23 17:19:07 -0300

    refatoracao: formatacao, implementacao de Type Hints

--- project.py
+++ project.py
@@ -1,11 +1,11 @@
-import sys
 import os
 import json
-
+import sys
 
 from PIL import Image, ImageDraw, ImageFont
 import requests
 from validator_collection import validators, checkers
+
 
 PASTA_TEMPLATES = "templates"
 PASTA_OUTPUT = "output"
@@ -13,7 +13,7 @@ ARQUIVO_CONFIG_JSON = "templates.json"
 FONTE_PADRAO = "ARIALLGT.TTF"
 
 
-def main():
+def main() -> None:
 
     escolha_usuario = get_escolha_usuario()
 
@@ -26,7 +26,7 @@ def main():
             sys.exit("Erro: Opção inválida. Programa encerrado.")
 
 
-def get_escolha_usuario():
+def get_escolha_usuario() -> int:
     while True:
         try:
             print(
@@ -42,7 +42,7 @@ def get_escolha_usuario():
             sys.exit("\nPrograma encerrado pelo usuario.")
 
 
-def novo_template():
+def novo_template() -> None:
     url_valida = obter_url_valida()
 
     nome_arquivo = obter_nome_arquivo()
@@ -65,7 +65,7 @@ def novo_template():
     print(f"Template '{nome_arquivo}' configurado com sucesso.")
 
 
-def obter_url_valida():
+def obter_url_valida() -> str:
     while True:
         url_input = input("Digite o URL da imagem que deseja usar como template: ")
         try:
@@ -78,7 +78,7 @@ def obter_url_valida():
             print("A URL está desformatada. Tente novamente.")
 
 
-def obter_nome_arquivo():
+def obter_nome_arquivo() -> str:
     while True:
         nome_arquivo = input("Digite o nome desejado para o novo arquivo: ").strip()
         if nome_arquivo:
@@ -87,7 +87,7 @@ def obter_nome_arquivo():
             print("O nome do arquivo não pode estar vazio. Tente novamente.")
 
 
-def baixar_imagem(url_valida):
+def baixar_imagem(url_valida: str) -> bytes | None:
     try:
         response = requests.get(url_valida)
         if response.status_code == 200:
@@ -104,7 +104,7 @@ def baixar_imagem(url_valida):
         return None
 
 
-def obter_configuracoes_texto():
+def obter_configuracoes_texto() -> dict[str, int | str] | None:
     while True:
         try:
             x_pos = int(input("Digite a coordenada X para o texto: "))
@@ -131,7 +131,7 @@ def obter_configuracoes_texto():
     }
 
 
-def salvar_imagem(caminho_salvamento, conteudo_imagem):
+def salvar_imagem(caminho_salvamento: str, conteudo_imagem: bytes) -> str | None:
     try:
         with open(caminho_salvamento, "wb") as meme_novo:
             meme_novo.write(conteudo_imagem)
@@ -141,7 +141,7 @@ def salvar_imagem(caminho_salvamento, conteudo_imagem):
         return None
 
 
-def registrar_template_json(nome_arquivo, caminho_arquivo, configuracoes_texto):
+def registrar_template_json(nome_arquivo: str, caminho_arquivo: str, configuracoes_texto: dict) -> None:
     novo_template_dados = {
         "x": configuracoes_texto["x"],
         "y": configuracoes_texto["y"],
@@ -164,19 +164,19 @@ def registrar_template_json(nome_arquivo, caminho_arquivo, configuracoes_texto):
         json.dump(dados_json, arquivo_json, indent=4)
 
 
-def construir_meme():
+def construir_meme() -> None:
     template_desejado, texto_meme = selecionar_template_texto()
 
-    x_position, y_position, tamanho_fonte, cor_texto, template_escolhido = carregar_template(template_desejado)
+    pos_x, pos_y, tamanho_fonte, cor_texto, template_escolhido = carregar_template(template_desejado)
 
     if template_escolhido:
-        criar_arquivo_output(template_escolhido, texto_meme, tamanho_fonte, cor_texto, x_position, y_position)
+        criar_arquivo_output(template_escolhido, texto_meme, tamanho_fonte, cor_texto, pos_x, pos_y)
         print(f"\nMeme do template {template_desejado} criado com sucesso!\n")
     else:
         print("Não foi possível criar o meme devido à ausência de configurações de template.")
 
 
-def selecionar_template_texto():
+def selecionar_template_texto() -> tuple[str, str]:
     pasta_templates = PASTA_TEMPLATES
     if not os.path.exists(pasta_templates):
         os.makedirs(pasta_templates)
@@ -220,7 +220,7 @@ def selecionar_template_texto():
                 print("Erro: Entrada inválida. Tente novamente.")
 
 
-def carregar_template(template_desejado):
+def carregar_template(template_desejado: str) -> tuple[int | None, int | None, int | None, str | None, str | None]:
     chave_template = formatar_template_inserido(template_desejado)
     caminho_configuracoes = ARQUIVO_CONFIG_JSON
 
@@ -244,13 +244,13 @@ def carregar_template(template_desejado):
     return (None, None, None, None, None)
 
 
-def criar_arquivo_output(template_escolhido, text, tamanho_fonte, cor_texto, x_position, y_position):
+def criar_arquivo_output(template_escolhido: str, text: str, tamanho_fonte: int, cor_texto: str, pos_x: int, pos_y: int) -> None:
     with Image.open(template_escolhido) as arquivo_template:
         draw = ImageDraw.Draw(arquivo_template)
         font = ImageFont.truetype(FONTE_PADRAO, size=tamanho_fonte)
 
         cor_em_rgb = converter_cor_para_rgb(cor_texto)
-        draw.text((x_position, y_position), text, fill=cor_em_rgb, font=font)
+        draw.text((pos_x, pos_y), text, fill=cor_em_rgb, font=font)
 
         nome_arquivo = os.path.basename(template_escolhido)
         nome_sem_extensao = os.path.splitext(nome_arquivo)[0]
@@ -262,18 +262,18 @@ def criar_arquivo_output(template_escolhido, text, tamanho_fonte, cor_texto, x_p
         arquivo_template.save(caminho_salvamento)
 
 
-def validar_cor(cor_input):
+def validar_cor(cor_input: str) -> str:
     cor_formatada = cor_input.strip().lower()
     if cor_formatada not in ["black", "white"]:
         raise ValueError("A cor deve ser 'black' ou 'white'")
     return cor_formatada
 
 
-def formatar_template_inserido(template_inserido):
+def formatar_template_inserido(template_inserido: str) -> str:
     return template_inserido.strip().lower().replace(" ", "_")
 
 
-def converter_cor_para_rgb(cor_texto):
+def converter_cor_para_rgb(cor_texto: str) -> tuple[int, int, int]:
     if cor_texto == "white":
         return (255, 255, 255)
     return (0, 0, 0)
